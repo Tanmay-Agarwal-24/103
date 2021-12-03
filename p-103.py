19db6f61c8ccb66f3e9ab19ff24517c20fb52d88
Tanmay-Agarwal-24 <80101441+Tanmay-Agarwal-24@users.noreply.github.com>
2021-12-03 17:45:32 +0530

    Add files via upload

---
+++ p-103.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+
+df=pd.read_csv("Copy+of+data+-+data.csv")
+fig=px.bar(df,x="date",y="cases",color="country",title="per capita income")
+fig.show()
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
